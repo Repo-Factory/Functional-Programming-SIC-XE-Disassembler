1c4acc9bed52dcde997cc03c9a3a17ff85bce7ba
Conner Sommerfield <conner.sommerfield@gmail.com>
2023-09-23 13:57:00 -0700

    Allow for Various text sections

--- main.cpp
+++ main.cpp
@@ -52,5 +52,5 @@ int main(int argc, char* argv[])
             recurseTextSection(textBytes - extractInfoToOutput(inputFile, outputFile, std::unique_ptr<Parser>(new Parser()))) :
             FileHandling::close(inputFile, outputFile);
     ;});
-    return recurseTextSection(FileHandling::locateTextSection(inputFile));  // Here we will actually call our lambda, which will execute bulk of program
+    while (!inputFile.eof()) recurseTextSection(FileHandling::locateTextSection(inputFile));  // Here we will actually call our lambda, which will execute bulk of program
 }
